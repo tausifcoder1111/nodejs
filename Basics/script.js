1368a4c25a8de5b8478f56ebd7631405403ef03f
tausifcoder1111 <tausifcoderr@gmail.com>
2026-03-22 14:47:43 +0530

    IIFE

--- Basics/script.js
+++ Basics/script.js
@@ -1,1 +1,18 @@
-console.log("Node JS")
+// const {add, subtract    } = require("./xyz")
+// console.log("Node JS")
+
+// console.log(globalThis === global) 
+
+// const sum = add(1,3)
+// console.log(sum)
+// const sub = subtract(10,1)
+// console.log(sub)
+
+// fawesome 
+
+
+(
+    function(){
+        console.log("IIFE- Immediately Invoked Function Expression")
+    }
+)();
---
+++ Basics/xyz.js
@@ -0,0 +1,4 @@
+const add = (a,b) => a+b;
+const subtract = function(a,b){return a-b;}
+
+module.exports = {add, subtract}
